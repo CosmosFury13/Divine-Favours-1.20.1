b66c5e0cb1c01246fc720baf7051e0ea68fe55d1
CosmosFury13 <oligee007@gmail.com>
2024-02-12 23:17:25 +0000

    -added the kalvis altar -added silver

---
+++ src/main/java/net/cosmosfury/divinefavours/block/kalvisaltar.java
@@ -0,0 +1,41 @@
+package net.cosmosfury.divinefavours.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import static net.minecraft.world.level.block.Blocks.DEEPSLATE_TILES;
+
+public class kalvisaltar extends HorizontalDirectionalBlock {
+
+    // You may need to adjust the constructor based on your needs
+    public kalvisaltar() {
+        super(Properties.of()
+                .strength(3.0F, 3.0F)
+                .noOcclusion()
+                .isViewBlocking((state, world, pos) -> false)
+                .isSuffocating((state, world, pos) -> false)
+                .isRedstoneConductor((state, world, pos) -> false)
+                .sound(SoundType.DEEPSLATE_TILES));
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+
+}
+
